5d14eb1f7faa76c14d333f73ce322c715af3275a
Geierwally <33550915+Geierwally-xD@users.noreply.github.com>
2018-01-25 05:58:31 +0000

    Update F3K_Setup.bat
    Bugfix transmitter drive-letter check on install batch: Exchanged apps folder with model folder at check right drive-letter for transmitters with not existing apps folder.

--- F3K_App/F3K_Setup.bat
+++ F3K_App/F3K_Setup.bat
@@ -6,7 +6,11 @@
 @echo connect your transmitter USB and type in the drive letter e.g. E
 @Set /P transmitter=SET transmitter:
 @echo ================================
-IF not exist %transmitter%:\apps\*.* goto  FAILPATH
+IF not exist %transmitter%:\Model\*.* goto  FAILPATH
+setlocal
+%transmitter%:
+IF not exist %transmitter%:\apps\*.* md apps
+endlocal
 @echo ================================
 @echo Partial transmitter backup required ?
 @echo ================================
